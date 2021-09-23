7a0400726afc36773572e4931449aef74036e2db
Erick Askim <erickaskim@gmail.com>
2021-09-23 13:06:05 -0700

    change to switch

--- IsIt5Yet.js
+++ IsIt5Yet.js
@@ -2,18 +2,38 @@ function isIt5Yet() {
   const now = new Date();
   const hours = now.getHours();
   const min = now.getMinutes();
-  if (hours >= 17) {
-    document.querySelector(
-      "p"
-    ).textContent = `Drink up me hartys YO HO! You could have been drinking for ${
-      hours - 17
-    } hours and ${min} minutes`;
-  } else {
-    if (hours <= 17) {
-      document.querySelector("p").textContent = "Have you thought about AA?";
-    }
+  switch (new Date().getHours()) {
+    default:
+      document.querySelector("#isIt5Yet").textContent =
+        "Have you thought about AA?";
+      break;
+    case 0:
+      document.querySelector("#isIt5Yet").textContent = "GO HOME!!!!";
+      break;
+    case 1:
+      document.querySelector("#isIt5Yet").textContent =
+        "It's 1am... sleep it off";
+      break;
+    case 2:
+      document.querySelector("#isIt5Yet").textContent =
+        "Seriously... It's 2am now... Get some sleep!";
+    case 3:
+      document.querySelector("#isIt5Yet").textContent =
+        "I know Matchbox20 said It's 3am somewhere... that somewhere is here and now, GO TO BED!";
+      break;
+    case 17:
+    case 18:
+    case 19:
+    case 20:
+    case 21:
+    case 22:
+    case 23:
+      document.querySelector(
+        "#isIt5Yet"
+      ).textContent = `Drink up me hartys YO HO! You could have been drinking for ${
+        hours - 17
+      } hours and ${min} minutes.`;
+      break;
   }
 }
 document.addEventListener("DOMContentLoaded", isIt5Yet);
-
-// doing this so i could put it in the pr
